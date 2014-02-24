b769b3491b60ef57b28967e1a24edc9c329287f1
dylan92 <dylanyates92@gmail.com>
2014-02-23 21:37:20 -0500

    Week 8 asset import 2

---
+++ Assets/Scripts/Events/Objective.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objective : MonoBehaviour {
+	[SerializeField] private Activateable[] targets;
+
+	public void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+			for (int i = 0; i < targets.Length; i++) {
+				targets[i].Activate();
+			}
+		}
+	}
+}
