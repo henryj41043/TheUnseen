d5c108da346d6e165ece82e15daedf638ede449d
Ryan Daugherty <sharmandra.ryan@gmail.com>
2014-02-25 03:21:42 -0500

    Made playerspawn

---
+++ Assets/Scripts/Player/CharacterStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStart : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		GameObject x = (GameObject) Instantiate (player, transform.position, transform.rotation);
+	}
+
+}
