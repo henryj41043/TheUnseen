da9980f6cdc6cd729294cabf3d646d09a3ee93d4
Ryan Daugherty <sharmandra.ryan@gmail.com>
2014-03-12 20:17:35 -0400

    Start of attractive sounds

--- Assets/Scripts/Creature/CreatureAI.cs
+++ Assets/Scripts/Creature/CreatureAI.cs
@@ -62,8 +62,12 @@ public class CreatureAI : MonoBehaviour {
 	private Animator anim;
 
 	CreatureSight vision;
-	
+
+	public double timeToWait = .1;
+	private double timeWaited;
+
 	void Awake() {
+		timeWaited = timeToWait;
 		vision = GetComponent<CreatureSight>();
 		lastPosMarker = new GameObject(name+"  Last Known Position Marker");
 		aiPath = GetComponent<AIPath>();
@@ -72,144 +76,148 @@ public class CreatureAI : MonoBehaviour {
 	}
 	
 	void Update () {
-		// Update Target
-		newTarget = GetNewestTarget();
+		timeWaited += Time.deltaTime;
+		if (timeWaited >= timeToWait){
+			timeWaited = 0;
+			// Update Target
+			newTarget = GetNewestTarget();
 
-		// Update State
-		if(currentState != States.Absorbing){
-			if (newTarget == null && currentTarget != null){
-				currentState = States.ChaseLastKnown;
-			}else if (newTarget != null && newTarget.tag == "Player"){
-				currentState = States.ChasePlayer;
-			}else if (newTarget != null){
-				currentState = States.ChasePOI;
-			}else if (newTarget == null && currentTarget == null &&  currentState == States.ChasePOI){
-				currentState = States.ChaseLastKnown;
-			}else if (newTarget == null && currentTarget == null && currentState != States.ChaseLastKnown && currentState != States.Searching){
-				currentState = States.Wander;
-			}
-		}
-
-		// Check if Enraged
-		if(isSuperSayian){
-			enragedMarker.SetActive(true);
-			actualWalkSpeed = walkSpeed*superSayianSpeedMult;
-			actualRunSpeed = runSpeed*superSayianSpeedMult;
-			actualChaseSpeed = chaseSpeed*superSayianSpeedMult;
-			superSayianTimer += Time.deltaTime;
-			if(superSayianTimer > superSayianTime){
-				superSayianCharge -= SSChargeDecrease;
-			}
-			if(superSayianCharge <= 0){
-				isSuperSayian = false;
-				superSayianCharge = 0;
-				superSayianTimer = 0;
-			}
-
-		}else{
-			enragedMarker.SetActive(false);
-			actualWalkSpeed = walkSpeed;
-			actualRunSpeed = runSpeed;
-			actualChaseSpeed = chaseSpeed;
-		} 
-
-		if (newTarget != null){
-			lastPosMarker.transform.position = newTarget.transform.position;
-		}
-
-		// Check State
-		if (currentState == States.Wander) {
-			aiPath.speed = actualWalkSpeed;
-			if(aiPath.target == null) {
-				GetNearestWaypoint();
-				aiPath.target = currentWaypoint.transform;
-			}else if(Vector3.Distance(transform.position, aiPath.target.transform.position) < minWaypointRange) {
-				if (currentWaypoint != null && currentWaypoint.GetComponent<Waypoint>() != null){
-					currentWaypoint = currentWaypoint.GetComponent<Waypoint>().NextWaypoint();
-				}else{
-					GetNearestWaypoint ();
-				}
-				aiPath.target = currentWaypoint.transform;
-			}
-		}else if(currentState == States.ChasePOI){
-			currentTarget = newTarget;
-			if (currentTarget != null){
-				aiPath.speed = actualRunSpeed;
-				aiPath.target = currentTarget.transform;
-				if(Vector3.Distance(transform.position, currentTarget.transform.position) < drainRange && CanDrain(currentTarget)){
-					currentState = States.Absorbing;
+			// Update State
+			if(currentState != States.Absorbing){
+				if (newTarget == null && currentTarget != null){
+					currentState = States.ChaseLastKnown;
+				}else if (newTarget != null && newTarget.tag == "Player"){
+					currentState = States.ChasePlayer;
+				}else if (newTarget != null){
+					currentState = States.ChasePOI;
+				}else if (newTarget == null && currentTarget == null &&  currentState == States.ChasePOI){
+					currentState = States.ChaseLastKnown;
+				}else if (newTarget == null && currentTarget == null && currentState != States.ChaseLastKnown && currentState != States.Searching){
+					currentState = States.Wander;
 				}
 			}
-		}else if(currentState == States.ChasePlayer){
-			currentTarget = newTarget;
-			if (currentTarget != null){
-				aiPath.speed = actualChaseSpeed;
-				aiPath.target = currentTarget.transform;
-				if(Vector3.Distance(transform.position, currentTarget.transform.position) < attackRange) {
-					if (!isAttacking) {
-						soundSource.clip = attackSound;
-						if (soundSource.isPlaying == false) {
-							soundSource.Play();
-						}
-						StartCoroutine(Attack(currentTarget));
+
+			// Check if Enraged
+			if(isSuperSayian){
+				enragedMarker.SetActive(true);
+				actualWalkSpeed = walkSpeed*superSayianSpeedMult;
+				actualRunSpeed = runSpeed*superSayianSpeedMult;
+				actualChaseSpeed = chaseSpeed*superSayianSpeedMult;
+				superSayianTimer += Time.deltaTime;
+				if(superSayianTimer > superSayianTime){
+					superSayianCharge -= SSChargeDecrease;
+				}
+				if(superSayianCharge <= 0){
+					isSuperSayian = false;
+					superSayianCharge = 0;
+					superSayianTimer = 0;
+				}
+
+			}else{
+				enragedMarker.SetActive(false);
+				actualWalkSpeed = walkSpeed;
+				actualRunSpeed = runSpeed;
+				actualChaseSpeed = chaseSpeed;
+			} 
+
+			if (newTarget != null){
+				lastPosMarker.transform.position = newTarget.transform.position;
+			}
+
+			// Check State
+			if (currentState == States.Wander) {
+				aiPath.speed = actualWalkSpeed;
+				if(aiPath.target == null) {
+					GetNearestWaypoint();
+					aiPath.target = currentWaypoint.transform;
+				}else if(Vector3.Distance(transform.position, aiPath.target.transform.position) < minWaypointRange) {
+					if (currentWaypoint != null && currentWaypoint.GetComponent<Waypoint>() != null){
+						currentWaypoint = currentWaypoint.GetComponent<Waypoint>().NextWaypoint();
+					}else{
+						GetNearestWaypoint ();
+					}
+					aiPath.target = currentWaypoint.transform;
+				}
+			}else if(currentState == States.ChasePOI){
+				currentTarget = newTarget;
+				if (currentTarget != null){
+					aiPath.speed = actualRunSpeed;
+					aiPath.target = currentTarget.transform;
+					if(Vector3.Distance(transform.position, currentTarget.transform.position) < drainRange && CanDrain(currentTarget)){
+						currentState = States.Absorbing;
 					}
 				}
-			}
-		}else if(currentState == States.ChaseLastKnown){
-			aiPath.target = lastPosMarker.transform;
-			if(progressTimer == 0f){
-				lastProgress = (lastPosMarker.transform.position - transform.position).magnitude;
-			}
-			progressTimer += Time.deltaTime;
-			if(progressTimer > checkProgressTime){
-				currentProgress = (lastPosMarker.transform.position - transform.position).magnitude;
-				if(currentProgress >= lastProgress){
+			}else if(currentState == States.ChasePlayer){
+				currentTarget = newTarget;
+				if (currentTarget != null){
+					aiPath.speed = actualChaseSpeed;
+					aiPath.target = currentTarget.transform;
+					if(Vector3.Distance(transform.position, currentTarget.transform.position) < attackRange) {
+						if (!isAttacking) {
+							soundSource.clip = attackSound;
+							if (soundSource.isPlaying == false) {
+								soundSource.Play();
+							}
+							StartCoroutine(Attack(currentTarget));
+						}
+					}
+				}
+			}else if(currentState == States.ChaseLastKnown){
+				aiPath.target = lastPosMarker.transform;
+				if(progressTimer == 0f){
+					lastProgress = (lastPosMarker.transform.position - transform.position).magnitude;
+				}
+				progressTimer += Time.deltaTime;
+				if(progressTimer > checkProgressTime){
+					currentProgress = (lastPosMarker.transform.position - transform.position).magnitude;
+					if(currentProgress >= lastProgress){
+						currentState = States.Wander;
+						currentTarget = null;
+						aiPath.target = null;
+						newTarget = null;
+					}
+					progressTimer = 0f;
+				}
+				if(Vector3.Distance(transform.position, lastPosMarker.transform.position) < minWaypointRange){
+					currentState = States.Searching;
+					timeSearchedSoFar = 0;
+					aiPath.target = null;
+				}
+			}else if(currentState == States.Absorbing){
+				soundSource.clip = absorbingSound;
+				if (soundSource.isPlaying == false) {
+					soundSource.Play();
+				}
+				isDraining = true;
+				if(currentTarget != null && CanDrain(currentTarget)){
+					if(Vector3.Distance(transform.position, currentTarget.transform.position) > drainRange){
+						currentState = States.ChasePOI;
+					}else{
+						currentTarget.GetComponent<Drainable>().Drain(ratioDrainPerSecond*Time.deltaTime);
+						superSayianCharge += ratioDrainPerSecond*Time.deltaTime;
+					}
+				}else{
+					isDraining = false;
 					currentState = States.Wander;
 					currentTarget = null;
 					aiPath.target = null;
 					newTarget = null;
 				}
-				progressTimer = 0f;
-			}
-			if(Vector3.Distance(transform.position, lastPosMarker.transform.position) < minWaypointRange){
-				currentState = States.Searching;
-				timeSearchedSoFar = 0;
-				aiPath.target = null;
-			}
-		}else if(currentState == States.Absorbing){
-			soundSource.clip = absorbingSound;
-			if (soundSource.isPlaying == false) {
-				soundSource.Play();
-			}
-			isDraining = true;
-			if(currentTarget != null && CanDrain(currentTarget)){
-				if(Vector3.Distance(transform.position, currentTarget.transform.position) > drainRange){
-					currentState = States.ChasePOI;
-				}else{
-					currentTarget.GetComponent<Drainable>().Drain(ratioDrainPerSecond*Time.deltaTime);
-					superSayianCharge += ratioDrainPerSecond*Time.deltaTime;
+				if(superSayianCharge >= 1){
+					isSuperSayian = true;
 				}
-			}else{
-				isDraining = false;
-				currentState = States.Wander;
-				currentTarget = null;
-				aiPath.target = null;
-				newTarget = null;
+			}else if (currentState == States.Searching){
+				timeSearchedSoFar += Time.deltaTime;
+				if (timeSearchedSoFar > searchTime){
+					currentState = States.Wander;
+					currentTarget = null;
+					aiPath.target = null;
+					newTarget = null;
+				}
 			}
-			if(superSayianCharge >= 1){
-				isSuperSayian = true;
-			}
-		}else if (currentState == States.Searching){
-			timeSearchedSoFar += Time.deltaTime;
-			if (timeSearchedSoFar > searchTime){
-				currentState = States.Wander;
-				currentTarget = null;
-				aiPath.target = null;
-				newTarget = null;
-			}
-		}
 
-		UpdateAnimations();
+			UpdateAnimations();
+		}
 	}
 
 	void UpdateAnimations(){
@@ -348,6 +356,9 @@ public class CreatureAI : MonoBehaviour {
 		}
 		if(obj.tag == "FiredOrb"){
 			return 1f*(obj.transform.position-transform.position).magnitude;
+		}
+		if(obj.tag == "AttractiveSound"){
+			return .001f*(obj.transform.position-transform.position).magnitude*obj.GetComponent<AttractiveSound>().range;
 		}
 		return 0;
 	}
--- Assets/Scripts/Sound/AttractiveSound.cs
+++ Assets/Scripts/Sound/AttractiveSound.cs
@@ -4,14 +4,13 @@ using System.Collections;
 public class AttractiveSound : MonoBehaviour {
 
 	public float range = 10000f;
+	public float lifespan = 1f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		lifespan -= Time.deltaTime;
+		if (lifespan <= 0){
+			Destroy (gameObject);
+		}
 	}
 }
