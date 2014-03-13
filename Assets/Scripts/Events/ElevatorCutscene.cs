88c39e5d408298e9be68e3b4a024724188bf94d3
dylan92 <dylanyates92@gmail.com>
2014-03-13 00:48:30 -0400

    Elevator cutscene

---
+++ Assets/Scripts/Events/ElevatorCutscene.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorCutscene : MonoBehaviour {
+	public CameraBob camera;
+	public GameObject normalLight;
+	public GameObject emergencyLight;
+	public GameObject[] sparks;
+	public AudioClip bangOnElevator;
+	public bool isDone = false;
+
+	public IEnumerator PlayCutscene() {
+		audio.PlayOneShot(bangOnElevator);
+		camera.shaking = true;
+		float start = Time.time;
+		float end = Time.time + 1.0f;
+		while (Time.time < end) {
+			camera.shakeAmplitudeX = (1.0f - ((Time.time - start) / (end - start))) * 0.5f;
+			camera.shakeAmplitudeY = (1.0f - ((Time.time - start) / (end - start))) * 0.5f;
+			yield return new WaitForSeconds(0.1f);
+		}
+		camera.shaking = false;
+		normalLight.SetActive(false);
+		emergencyLight.SetActive(true);
+		foreach (GameObject g in sparks) {
+			g.SetActive(true);
+		}
+		yield return new WaitForSeconds(3.0f);
+		audio.PlayOneShot(bangOnElevator);
+		emergencyLight.SetActive(false);
+		camera.shaking = true;
+		start = Time.time;
+		end = Time.time + 1.0f;
+		while (Time.time < end) {
+			camera.shakeAmplitudeX = (1.0f - ((Time.time - start) / (end - start))) * 0.5f;
+			camera.shakeAmplitudeY = (1.0f - ((Time.time - start) / (end - start))) * 0.5f;
+			yield return new WaitForSeconds(0.1f);
+		}
+		camera.shaking = false;
+		isDone = true;
+	}
+}
